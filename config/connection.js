907421e3dc0e0992f7a8179127806e4254b58b6d
Steve Calla <callasteven@gmail.com>
2022-10-20 21:41:08 -0600

    Adjust port to PORT

--- config/connection.js
+++ config/connection.js
@@ -5,8 +5,9 @@ require("dotenv").config();
 // Node will look for this environment variable and if it exists, it will use it. Otherwise, it will assume that you are running this application locally
 
 const connectionString =
-  // process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/studentsDB';
   process.env.MONGODB_URI || process.env.DB_URI;
+  // process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/studentsDB';
+  // 'mongodb+srv://callasteven:4184Longview@cluster0.x8hemhd.mongodb.net/studentsDB?retryWrites=true&w=majority'
 
 connect(connectionString, {
   useNewUrlParser: true,
